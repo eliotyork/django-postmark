56930863324091806e291db3ef9cc74f5ad49b4f
Tommy <tommy@Tommys-MacBook-Air-2.local>
2011-09-09 15:06:37 +0200

    fixing mysql and pytz problem.

--- postmark/models.py
+++ postmark/models.py
@@ -106,7 +106,8 @@ def sent_message(sender, **kwargs):
         tz_offset = int(tz.split(":", 1)[0])
         tz = timezone("Etc/GMT%s%d" % ("+" if tz_offset >= 0 else "-", tz_offset))
         submitted_at = tz.localize(datetime.strptime(timestamp[:26], POSTMARK_DATETIME_STRING)).astimezone(pytz.utc)
-        
+        #toumhi patch here because mysql and pytz don't go well together
+        submitted_at = submitted_at.replace(tzinfo=None)
         
         emsg = EmailMessage(
             message_id=resp["MessageID"],
